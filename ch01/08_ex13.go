b6d5bf4d5d4be8da1e4b102923148bc7fa4a2563
solareenlo <solareenlo@gmail.com>
2021-07-01 05:42:42 +0900

    fix: ex13

--- ch01/08_ex13.go
+++ ch01/08_ex13.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"time"
 )
 
 func main() {
 	start := time.Now()
 	s := ""
-	for i := 0; i < 10000; i++ {
-		s += strconv.Itoa(i)
+	num := make([]string, 100000, 100000)
+	for _, i := range num[:] {
+		s += i
+		fmt.Println(s)
 	}
 	fmt.Println(time.Since(start).Seconds())
 }
--- ch01/09_ex13.go
+++ ch01/09_ex13.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	start := time.Now()
-	num := make([]string, 10000, 10000)
-	strings.Join(num[:], " ")
+	num := make([]string, 100000, 100000)
+	fmt.Println(strings.Join(num[:], " "))
 	fmt.Println(time.Since(start).Seconds())
 }
